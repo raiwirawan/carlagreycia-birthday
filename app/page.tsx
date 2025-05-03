8f0d5132f9b19d2d422489d2905275bc8d454171
unknown <raiadi5566@gmail.com>
2025-05-03 19:32:55 +0800

    Fix suspense useSearchParam

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
+"use client";
+
+import { Suspense } from "react";
 import WelcomeSection from "@/components/welcome-section";
 
 export default function Home() {
 	return (
-		<main>
-			<WelcomeSection />
-		</main>
+		<Suspense
+			fallback={
+				<div
+					style={{
+						textAlign: "center",
+						padding: "50px",
+						fontFamily: "'Poppins', cursive",
+						color: "#333",
+					}}
+				>
+					<h1 style={{ fontSize: "3rem" }}>Loading...</h1>
+				</div>
+			}
+		>
+			<main>
+				<WelcomeSection />
+			</main>
+		</Suspense>
 	);
 }
